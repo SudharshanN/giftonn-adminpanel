1df1d60b775585a4a7eb4d2a7f81906f82e3a577
SaiChinnu17 <masimukkusaikrishna17@gmail.comm>
2022-01-06 20:23:44 +0530

    init

--- src/app/vender-order/vender-order.component.ts
+++ src/app/vender-order/vender-order.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from "@angular/material/dialog";
+import { Router } from "@angular/router";
+import { DeleteModelComponent } from "../delete-model/delete-model.component";
+import { FilterComponent } from "../filter/filter.component";
 
 @Component({
   selector: 'app-vender-order',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VenderOrderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
+  download(type) {
+    console.log(type);
+  }
+  deleteModel() {
+    const dialogRef = this.dialog.open(DeleteModelComponent, {
+      width: "25%",
+      data: "user",
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+  openFilter() {
+    const dialogRef = this.dialog.open(FilterComponent, {
+      width: "50%",
+      data: { filterType: "user" },
+    });
+  }
+
 
 }
